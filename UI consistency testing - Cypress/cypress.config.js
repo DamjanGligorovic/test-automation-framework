dcbfc255886f572d19a084eaabee55886aae9a2c
DamjanGligorovic <damjangligorovic@hotmail.com>
2024-08-12 11:44:51 +0200

    Add files via upload

---
+++ UI consistency testing - Cypress/cypress.config.js
@@ -0,0 +1,37 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'unui9u',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: "cypress/tests/webdriver_uni/**/*.{js,jsx,ts,tsx,feature}",
+    excludeSpecPattern: "cypress/tests/other/*.js",
+    baseUrl: "https://magento.softwaretestingboard.com/",
+    video: true,
+ 
+    reporter: 'cypress-multi-reporters',
+    reporterOptions: {
+      configFile: 'reporter-config.json'
+    },
+    chromeWebSecurity: false,
+    experimentalSessionAndOrigin: true,
+    defaultCommandTimeout: 50000,
+    pageLoadTimeout: 240000,
+    screenshotOnRunFailure: true,
+    trashAssetsBeforeRuns: true,
+    retries:{
+      runMode: 0,
+      openMode: 1,
+    },
+    env: {
+      
+      //webdriveruni_homepage: "http://www.webdriveruniversity.com",
+      pcpress_homepage: "https://pcpress.rs/prodavnica/index.php",
+      sasomange_registrationpage: "https://sasomange.rs/registracija",
+      sasomange_loginpage: "https://sasomange.rs/uloguj-se",
+      magento_homepage:"https://magento.softwaretestingboard.com/"
+    }
+  },
+});
